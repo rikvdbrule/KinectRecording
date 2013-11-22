fb3b16ee6319c96f9081246b6e88fcffd6b158ff
rikvdbrule <rikvdbrule@gmail.com>
2013-11-22 15:06:43 +0100

    Added record functions header and implementation

---
+++ KinectExplorer-D2D/RecordFunctions.cpp
@@ -0,0 +1,27 @@
+#include "RecordFunctions.h"
+#include <string>
+
+#define BUFFER_SIZE 80
+#define TIMESTAMP_FORMAT "%Y%m%d-%H.%M.%S"
+
+tm * CreateTimeStruct()
+{
+	time_t now = time(0);
+    return localtime(&now);
+}
+
+std::string CreateDateTimeStamp()
+{
+    char buff[BUFFER_SIZE];
+	struct tm * loctime = CreateTimeStruct();
+    strftime(buff, BUFFER_SIZE, TIMESTAMP_FORMAT, loctime);
+    std::string timestamp = buff;
+	return timestamp;
+}
+
+std::string GenerateSkeletonOutputFileName()
+{
+	const std::string prefix = "../skeleton_out_";
+	const std::string affix = ".data";
+	return prefix + CreateDateTimeStamp() + affix;
+}
